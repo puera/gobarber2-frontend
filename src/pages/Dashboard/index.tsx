5051960e51d270839372f5a27c7dccc8fae025a0
Renann Souza <renann_puera@hotmail.com>
2020-05-22 17:16:08 -0300

    List Appointments

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FiPower, FiClock } from 'react-icons/fi';
 import {
@@ -9,12 +9,15 @@ import {
   Content,
   Schedule,
   NextAppointment,
+  Section,
+  Appointment,
   Calendar,
 } from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
   const { signOut, user } = useAuth();
   return (
     <Container>
@@ -58,6 +61,56 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </NextAppointment>
+
+          <Section>
+            <strong>Manhã</strong>
+
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/1524415?s=460&u=e16af1a103dc3f848419d24f2dee94d0b3eeca10&v=4"
+                  alt="Renann Souza"
+                />
+                <strong>Renann Souza</strong>
+              </div>
+            </Appointment>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/1524415?s=460&u=e16af1a103dc3f848419d24f2dee94d0b3eeca10&v=4"
+                  alt="Renann Souza"
+                />
+                <strong>Renann Souza</strong>
+              </div>
+            </Appointment>
+          </Section>
+          <Section>
+            <strong>Tarde</strong>
+            <Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars2.githubusercontent.com/u/1524415?s=460&u=e16af1a103dc3f848419d24f2dee94d0b3eeca10&v=4"
+                  alt="Renann Souza"
+                />
+                <strong>Renann Souza</strong>
+              </div>
+            </Appointment>
+          </Section>
         </Schedule>
         <Calendar />
       </Content>
